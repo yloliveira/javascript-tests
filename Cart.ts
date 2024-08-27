65559e7e19abbbd3423b86397039c943d035780b
Yan Oliveira <oliveira.yan02@gmail.com>
2024-08-27 10:05:20 -0300

    feat(shopping-cart): should add new products and return the correct cart total value

--- Cart.ts
+++ Cart.ts
@@ -1,5 +1,24 @@
+type Product = {
+  title: string;
+  price: number;
+};
+
+type Item = {
+  product: Product;
+  quantity: number;
+};
+
+const items: Item[] = [];
+
 export default class Cart {
   getTotal() {
-    return 0;
+    return items.reduce(
+      (acc, cur) => acc + cur.product.price * cur.quantity,
+      0
+    );
+  }
+
+  addProduct(product: Product, quantity: number) {
+    items.push({ product, quantity });
   }
 }
